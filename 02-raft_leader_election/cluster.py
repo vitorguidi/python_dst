cdd9be887614d088167910bdeba4c1d01af9790a
Vitor Guidi <vitorguidi@gmail.com>
2024-10-06 15:16:49 -0300

    Spliting scheduler in io_uring and task tick, create cluster class

---
+++ 02-raft_leader_election/cluster.py
@@ -0,0 +1,31 @@
+from commons import Clock, RNG
+from raft import RaftNode
+from dispatcher import Dispatcher
+from scheduler import Scheduler
+from io_uring import IOUring
+from task import Task
+
+class Cluster:
+    def __init__(self, nr_nodes, seed):
+        self._clock = Clock()
+        self._rng = RNG(seed)
+        self._nodes = []
+        for i in range(nr_nodes):
+            node = RaftNode(i, self._clock, nr_nodes)
+            self._nodes.append(node)
+        dispatcher = Dispatcher(self._nodes)
+        self._sched = Scheduler(IOUring(), dispatcher, self._clock)
+        for node in self._nodes:
+            self._sched.add_task([Task(node.raft_loop(), None, f'node_{node._node_id}_raft_loop)'), None])
+
+    def start(self, tick_limit = None):
+        tick = 0
+        while True:
+            if tick_limit is not None and tick > tick_limit:
+                break
+            self._sched.tick()
+            tick+=1
+
+if __name__ == '__main__':
+    raft_cluster = Cluster(3, 0)
+    raft_cluster.start(tick_limit = 1000000)
--- 02-raft_leader_election/scheduler.py
+++ 02-raft_leader_election/scheduler.py
@@ -12,66 +12,73 @@ class Scheduler:
         self._dispatcher = dispatcher
         self._clock = clock
 
-
     def add_task(self, task):
         self._tasks.append(task)
 
-    # Assumption: we only yield to ask for syscalls, and always return a value
-    # in the end. Coroutines are a narrower set than generators
-    def run(self):
-        while self._tasks or not self._io_uring.is_empty():
-            [task, arg] = self._tasks.pop(0)
-            assert type(task) == Task
-            print(f'dequeued task = {task._name}')
-            try:
-                result = task.run(arg)
-                self._clock.tick()
-                print(f'got result {result} running task {task._name}')
-                # We only allow nodes to request RPCs, launch tasks or 
-                # return a value
-                if isinstance(result, Request):
-                    print(f'got request{result}')
-                    # I need to bind current task value NOW otherwise this will bind
-                    # to another value when coroutine stack dances around
-                    def cqe_to_original_task_callback_factory(task):
-                        return lambda cqe_data : self.add_task([task, cqe_data])
-                    # enqueue IO request, with the proper callback to resume once CQE gets popped
-                    sqe = SQE(result, cqe_to_original_task_callback_factory(task))
-                    print(f'enqueuing {sqe} for task {task._name} as CQE')
-                    self._io_uring.enqueue_sqe(sqe)
-                elif isinstance(result, Task):
-                    def return_value_to_task_callback_factory(requester_task):
-                        return lambda retval : self.add_task([requester_task, retval])
-                    result._callback = return_value_to_task_callback_factory(task)
-                    self.add_task([result, None])
-                else:
-                    print(f'Adding task back to the scheduler: {task._name}')
-                    # Every coroutine that finishes is an RPC handler
-                    # This is a sanity check
-                    # Other coroutines are the raft loops, which should never stop
-                    self.add_task([task, None])
-            except StopIteration:
-                task.callback()
-            cqe = self._io_uring.dequeue_cqe()
-            if cqe:
-            # add task with result back to task queue
-                cqe.callback(cqe.data)
-            sqe = self._io_uring.dequeue_sqe()
-            if sqe:
-                print(f'dequeued sqe {sqe}')
-            # Spawn a task that will create a CQE whose callback gives data to originator coro
-                req = sqe.data
-                assert isinstance(req, Request)
-                # Bind this NOW, otherwise the value of sqe or task will change
-                # Once the coroutine leaves and comes back to this stack
-                def enqueue_cqe_callback_factory(sqe_callback):
-                    return lambda x : self._io_uring.enqueue_cqe(CQE(x, sqe_callback))
-                callback = enqueue_cqe_callback_factory(sqe.callback) 
-                assert sqe is not None
-                target_node = req.node_to
-                rpc_name = req.rpc_name
-                handler = self._dispatcher.get_handler_coro(req.rpc_name, req.node_to)
-                io_task = Task(handler, callback, f'handle_{rpc_name}_rpc on node {target_node}')
-                print(f'Enqueing IO task for SQE {sqe}')
-                self.add_task([io_task, req])
-                print(f'Enqueued task {io_task._name}')
+    def tick(self):
+        self._tick_tasks()
+        self._tick_iouring()
+        self._clock.tick()
+
+    def _tick_iouring(self):
+        if self._io_uring.is_empty():
+            return
+        cqe = self._io_uring.dequeue_cqe()
+        if cqe:
+        # add task with result back to task queue
+            cqe.callback(cqe.data)
+        sqe = self._io_uring.dequeue_sqe()
+        if sqe:
+            print(f'dequeued sqe {sqe}')
+        # Spawn a task that will create a CQE whose callback gives data to originator coro
+            req = sqe.data
+            assert isinstance(req, Request)
+            # Bind this NOW, otherwise the value of sqe or task will change
+            # Once the coroutine leaves and comes back to this stack
+            def enqueue_cqe_callback_factory(sqe_callback):
+                return lambda x : self._io_uring.enqueue_cqe(CQE(x, sqe_callback))
+            callback = enqueue_cqe_callback_factory(sqe.callback) 
+            assert sqe is not None
+            target_node = req.node_to
+            rpc_name = req.rpc_name
+            handler = self._dispatcher.get_handler_coro(req.rpc_name, req.node_to)
+            io_task = Task(handler, callback, f'handle_{rpc_name}_rpc on node {target_node}')
+            print(f'Enqueing IO task for SQE {sqe}')
+            self.add_task([io_task, req])
+            print(f'Enqueued task {io_task._name}')       
+
+    def _tick_tasks(self):
+        if not self._tasks:
+            return
+        [task, arg] = self._tasks.pop(0)
+        assert type(task) == Task
+        print(f'dequeued task = {task._name}')
+        try:
+            result = task.run(arg)
+            self._clock.tick()
+            print(f'got result {result} running task {task._name}')
+            # We only allow nodes to request RPCs, launch tasks or 
+            # return a value
+            if isinstance(result, Request):
+                print(f'got request{result}')
+                # I need to bind current task value NOW otherwise this will bind
+                # to another value when coroutine stack dances around
+                def cqe_to_original_task_callback_factory(task):
+                    return lambda cqe_data : self.add_task([task, cqe_data])
+                # enqueue IO request, with the proper callback to resume once CQE gets popped
+                sqe = SQE(result, cqe_to_original_task_callback_factory(task))
+                print(f'enqueuing {sqe} for task {task._name} as CQE')
+                self._io_uring.enqueue_sqe(sqe)
+            elif isinstance(result, Task):
+                def return_value_to_task_callback_factory(requester_task):
+                    return lambda retval : self.add_task([requester_task, retval])
+                result._callback = return_value_to_task_callback_factory(task)
+                self.add_task([result, None])
+            else:
+                print(f'Adding task back to the scheduler: {task._name}')
+                # Every coroutine that finishes is an RPC handler
+                # This is a sanity check
+                # Other coroutines are the raft loops, which should never stop
+                self.add_task([task, None])
+        except StopIteration:
+            task.callback()
